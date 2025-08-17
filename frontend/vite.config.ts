582df05ee4e033788caabf47a709f0cdbb5eeed0
davidscottmills <vwdsm14@gmail.com>
2025-08-17 18:28:02 -0500

    feat(tmpl): actually get template to generate

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
 })
